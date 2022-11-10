96c33fc1a505f79ac7a132792d7a847b5bb80935
Katarzyna Zamaria <katarzyna.zamaria@hexagon.com>
2022-11-10 17:28:19 +0100

    [home]
    Added unit tests for Home component
    Fixed Article List component tests

--- src/app/article-list/article-list.component.spec.ts
+++ src/app/article-list/article-list.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { ArticleService } from '../article.service';
 
 import { ArticleListComponent } from './article-list.component';
 
 describe('ArticleListComponent', () => {
   let component: ArticleListComponent;
   let fixture: ComponentFixture<ArticleListComponent>;
+  let articleServiceStub;
 
   beforeEach(async () => {
+
+    articleServiceStub = {
+      articles: of([])
+    }
+
     await TestBed.configureTestingModule({
-      declarations: [ ArticleListComponent ]
+      declarations: [ ArticleListComponent ],
+      providers: [
+        { provide: ArticleService, useValue: articleServiceStub }
+      ]
     })
     .compileComponents();
   });
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,19 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from '../auth.service';
+import { Observable, of } from 'rxjs';
 
 import { HeaderComponent } from './header.component';
+import { User } from '../user';
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let authServiceStub: { loggedUser: Observable<User | null>};
 
   beforeEach(async () => {
+
+    authServiceStub = {
+      loggedUser: of({ bio: '', email: '', image: '', token: '', username: 'John Doe' })
+    }
+
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      providers: [ 
+        { provide: AuthService, useValue: authServiceStub } 
+      ],
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(() => {    
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +35,23 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should display Login In menu item when user is not logged in', () => {
+    authServiceStub.loggedUser = of(null);
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    let loginLink = fixture.nativeElement.querySelector('#loginLink');
+    expect(loginLink.textContent).toEqual('Log In')
+  })
+
+  it('should display username menu item when user is logged in', () => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    let profileLink = fixture.nativeElement.querySelector('#profileLink');
+    expect(profileLink.textContent).toEqual('Hello John Doe')
+  })
+
 });
